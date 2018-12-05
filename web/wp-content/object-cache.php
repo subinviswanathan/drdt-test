a07d9071c0610257f42673c9c90cac7a184e5a48
Facundo Farias <contacto@facundofarias.com.ar>
2018-12-05 15:43:15 -0300

    Fix comments for WPCS

--- web/wp-content/object-cache.php
+++ web/wp-content/object-cache.php
@@ -1,3 +1,13 @@
 <?php
-# This is a Windows-friendly symlink
+/**
+ * Redis Object Cache using WP Redis
+ *
+ * @link https://wordpress.org/plugins/wp-redis/
+ *
+ * @package platform
+ */
+
+/**
+ * Windows-friendly symlink
+ */
 require_once WP_CONTENT_DIR . '/plugins/wp-redis/object-cache.php';
