96b5b4e0f13916983d553751933e23caf85d7daf
Santhosh Kumar MJ <Santhosh.kumar@ness.com>
2018-12-21 16:17:30 +0530

    DRDT: Archive page along with homepage,article and content

--- web/wp-content/themes/bumblebee/archive.php
+++ web/wp-content/themes/bumblebee/archive.php
@@ -9,45 +9,79 @@
 
 get_header();
 ?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
-
-	<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
-
+<main class="archive-page">
+	<section class="advertisement">
+		<?php
+		bumblebee_render_ad(
+			uniqid( 'ad' ),
+			[
+				'slot-name' => 'prearticle',
+				'sizes'     => '970x250,970x90,728x90,3x3',
+				'targeting' => [
+					'pos'      => 'prearticle',
+					'location' => 'top',
+				],
+			]
+		);
+		?>
+	</section>
 	<?php
-	/* Start the Loop */
-	while ( have_posts() ) :
-		the_post();
-
-		/*
-		* Include the Post-Type-specific template for the content.
-		* If you want to override this in a child theme, then include a file
-		* called content-___.php (where ___ is the Post Type name) and that will be used instead.
-		*/
-		get_template_part( 'template-parts/content', get_post_type() );
-
-	endwhile;
-
-	the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
+	if ( have_posts() ) :
 	?>
+		<section class="archive-content">
+			<div class="archive-headings">
+				<div class="breadcrumbs">
+					<a href="#">JOBSITE TIPS</a> > <a href="#">BUSINESS</a>
+				</div>
+				<?php
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_description( '<div class="archive-description">', '</div>' );
+				?>
+			</div>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
+			<?php
+			the_post();
+			get_template_part( 'template-parts/archive/content', 'hero' );
+			?>
+			<ul class="featured-posts">
+				<?php
+				$i = 0;
+				while ( have_posts() && $i++ < 5 ) :
+					the_post();
+					get_template_part( 'template-parts/archive/content', 'featured' );
+					endwhile;
+				?>
+			</ul>
+		</section>
+		<?php $section_num = 0; ?>
+		<?php while ( have_posts() ) : ?>
+			<?php $section_num++; ?>
+			<section class="archive-content">
+				<div class="pure-g recipes">
+					<?php for ( $i = 0; $i < 8; $i++ ) : ?>
+					<?php if ( ( $wp_query->current_post + 1 ) !== ( $wp_query->post_count ) ) : ?>
+					<?php the_post(); ?>
+					<?php get_template_part( 'template-parts/archive/content', 'grid' ); ?>
+					<?php if ( 3 === $i ) { ?>
+					</div><div class="pure-g recipes">
+					<?php } ?>
+					<?php endif; ?>
+					<?php endfor; ?>
+				</div>
+			</section>
+			<section class="full-width-ad">
+				<?php
+				bumblebee_render_ad(
+					uniqid( 'ad' ),
+					[
+						'slot-name' => ( 1 === $section_num ? 'top' : 2 === $section_num ? 'middle' : 'scroll' ),
+						'sizes'     => '970x550,970x250,970x90,728x90,300x250,3x3',
+					]
+				);
+				?>
+			</section>
+		<?php endwhile; ?>
+	<?php endif; ?>
+</main>
 <?php
-get_sidebar();
 get_footer();
