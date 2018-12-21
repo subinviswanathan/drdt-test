4dd0088cb9f062e15f97fb1d85541a2fda09ecc9
Santhosh Kumar MJ <Santhosh.kumar@ness.com>
2018-12-21 16:19:15 +0530

    DRDT:Archive Content commit

---
+++ web/wp-content/themes/bumblebee/template-parts/archive/content-hero.php
@@ -0,0 +1,18 @@
+<?php
+/** Archive hero template
+
+ * @package bumblebee
+ */
+
+?>
+<div class="pure-g hero-container">
+	<a class="pure-u-sm-2-5 hero-image">
+		<?php echo get_the_post_thumbnail( $post_id, 'homepage-featured-big', array( 'class' => 'pure-img' ) ); ?>
+	</a>
+	<div class="pure-u-sm-3-5">
+		<div class="hero-text">
+			<?php the_title( '<h3 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h3>' ); ?>
+			<div class="hero-excerpt"><?php the_excerpt(); ?></div>
+		</div>
+	</div>
+</div>
