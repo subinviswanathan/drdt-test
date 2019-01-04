7c155c9be296f850defaafdba0b8c9eab942d399
Amrita <amrita.05chouhan@gmail.com>
2019-01-04 15:47:45 +0530

    DRDT- created separate social-share icons files, referring same from wherever needed.

--- web/wp-content/themes/bumblebee/single-listicle.php
+++ web/wp-content/themes/bumblebee/single-listicle.php
@@ -36,12 +36,8 @@ $category = $category[0];
 		?>
 	</section>
 	<section class="content pure-g">
-	<section class="social-share-bar-desktop pure-u-lg-2-24">
-		<ul class="pure-menu-list social-menu">
-			<li class="pure-menu-item"><a href="#" class="pure-menu-link"><img class="social-icons" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/images/envelope-regular.svg" /></a></li>
-			<li class="pure-menu-item"><a href="#" class="pure-menu-link"><img class="social-icons" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/images/facebook-f-brands.svg" /></a></li>
-			<li class="pure-menu-item"><a href="#" class="pure-menu-link"><img class="social-icons" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/images/pinterest-p-brands.svg" /></a></li>
-		</ul>
+	<section class="social-menu-desktop pure-u-lg-2-24">
+        <?php get_template_part( 'template-parts/social-share', 'none' ); ?>
 	</section>
 	<section class=" pure-u-1 pure-u-lg-14-24">
 		<div class="contentbarheader">
@@ -51,12 +47,8 @@ $category = $category[0];
 				<img src="http://cpt.test.rda.net/wp-content/uploads/sites/9/2018/10/Blue-Makeup-MAC-1200x675.jpg" class="author-image" alt="img"></img>
 				<span class="author-name">Amrita</span>
 			</div>
-			<section class="social-share-bar-mobile">
-				<ul class="pure-menu-list social-menu">
-					<li class="pure-menu-item"><a href="#" class="pure-menu-link"><img class="social-icons" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/images/envelope-regular.svg" /></a></li>
-					<li class="pure-menu-item"><a href="#" class="pure-menu-link"><img class="social-icons" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/images/facebook-f-brands.svg" /></a></li>
-					<li class="pure-menu-item"><a href="#" class="pure-menu-link"><img class="social-icons" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/images/pinterest-p-brands.svg" /></a></li>
-				</ul>
+			<section class="social-menu-mobile">
+				<?php get_template_part( 'template-parts/social-share', 'none' ); ?>
 			</section>
 			<div class="dek"><?php the_excerpt(); ?></div>
 		</div>
---
+++ web/wp-content/themes/bumblebee/template-parts/social-share.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: p7111439
+ * Date: 1/4/2019
+ * Time: 2:56 PM
+ */
+?>
+<ul class="pure-menu-list social-menu">
+	<li class="social-share-item"><a href="#" class="pure-menu-link"><img class="social-icons" src=" <?php echo esc_url( get_stylesheet_directory_uri() ); ?>/images/envelope-regular.svg" /></a></li>
+	<li class="social-share-item"><a href="#" class="pure-menu-link"><img class="social-icons" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/images/facebook-f-brands.svg" /></a></li>
+	<li class="social-share-item"><a href="#" class="pure-menu-link"><img class="social-icons" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/images/pinterest-p-brands.svg" /></a></li>
+</ul>
