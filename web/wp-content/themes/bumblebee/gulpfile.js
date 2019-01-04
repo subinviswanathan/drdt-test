eb937f4764c5273e8aa4a748fe75ff1715213052
Amrita <amrita.05chouhan@gmail.com>
2019-01-04 13:23:54 +0530

    DRDT- added gulp task for template, header, footer related style

--- web/wp-content/themes/bumblebee/gulpfile.js
+++ web/wp-content/themes/bumblebee/gulpfile.js
@@ -2,17 +2,29 @@
  
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var es = require('event-stream');
  
 sass.compiler = require('node-sass');
- 
+
 gulp.task('sass', function () {
-  return gulp.src('./sass/**/style.scss')
+  return gulp.src('./sass/**/style_main.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./'));
+});
+
+var component = ['header', 'homepage', 'listicle', 'article', 'archive', 'footer'];
+
+// create a css per feature
+gulp.task('saas:component', function () {
+	return es.merge(component.map(function (item) {
+		return gulp.src('./sass/' + item + '.scss')
+			.pipe(sass().on('error', sass.logError))
+			.pipe(gulp.dest('./'));
+	}));
 });
  
 gulp.task('sass:watch', function () {
   gulp.watch('./sass/**/*.scss', ['sass']);
 });
 
-gulp.task('default', ['sass']);
+gulp.task('default', ['sass','saas:component']);
