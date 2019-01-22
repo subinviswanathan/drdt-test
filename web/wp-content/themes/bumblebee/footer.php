f6d7114813de79e8d753506ebea2d70853b6a0f7
Chris Hurst <chris@iamchrishurst.com>
2019-01-22 11:58:43 -0500

    DRDT-22: Rewrote footer markup and styles

--- web/wp-content/themes/bumblebee/footer.php
+++ web/wp-content/themes/bumblebee/footer.php
@@ -109,6 +109,84 @@ wp_enqueue_style( 'bumblebee-style-footer', get_stylesheet_directory_uri() . '/f
 	</div>
 </footer>
 
+<footer class="footer" style="background-color: #424242;">
+	<div class="container">
+		<div class="pure-g">
+			<div class="pure-u-1 pure-u-md-3-5 left-foot">
+				<div class="ipad-hide logo-socials">
+					<a href=" <?php echo esc_url( site_url() ); ?> " class="footer-logo"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/images/cpt-footer-logo.svg" width="100px" alt="Construction Pro Tips Square Logo"></img></a>
+					<?php
+					if ( has_nav_menu( 'v2-footer-social-links' ) ) {
+						wp_nav_menu(
+							array(
+								'theme_location' => 'v2-footer-social-links',
+								'menu_class' => 'footer-social-links',
+								'container'  => false,
+							)
+						);
+					}
+					?>
+				</div> <!-- /.ipad-hide -->
+				<div class="site-links">
+					<?php
+					if ( has_nav_menu( 'v2-footer-site-links' ) ) {
+						wp_nav_menu(
+							array(
+								'theme_location' => 'v2-footer-site-links',
+								'menu_class' => 'footer-site-links',
+								'container'  => false,
+							)
+						);
+					}
+					?>
+				</div>
+				<div class="footer-brand-links-container">
+					<div class="footer-brand-links">
+						<p>Our Brands</p>
+						<?php
+						if ( has_nav_menu( 'v2-footer-brand-links' ) ) {
+							wp_nav_menu(
+								array(
+									'theme_location' => 'v2-footer-brand-links',
+									'menu_class'     => 'footer-brand-links',
+									'container'      => false,
+								)
+							);
+						}
+						?>
+					</div>
+					<?php
+					if ( has_nav_menu( 'v2-footer-global-links' ) ) {
+						wp_nav_menu(
+							array(
+								'theme_location' => 'v2-footer-global-links',
+								'menu_class'     => 'footer-global-links',
+								'container'      => false,
+							)
+						);
+					}
+					?>
+					<div class="copyright">&copy; <?php echo date( 'Y' ); ?> <?php echo esc_html( __( 'Home Service Publications, Inc', 'tmbi-theme-v3' ) ); ?></div>
+				</div>
+			</div>
+			<div class="pure-u-1 pure-u-md-2-5 right-foot">
+				<div class="newsletter">
+					<h3 class="">Sign Up For Our Newsletter</h3>
+					<form action="<?php echo esc_url( get_site_url() ); ?>/newslettersignuppage/" method="post">
+						<input type="text" id="email" placeholder="Email Address"></input>
+						<button type="submit" id="subscribe">Sign Up</button>
+					</form>
+				</div>
+				<div class="diyu-logo">
+					<a href="https://www.mydiyuniversity.com/" target="_blank" rel="noopener noreferrer">
+						<img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/images/diyu-header-sticky.svg" width="300px" alt="DIY University Logo"></img>
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
+
 <?php wp_footer(); ?>
 
 </body>
