a4e944c34200fd0d2020a38f06ed4d2c59fdad67
Facundo Farias <contacto@facundofarias.com.ar>
2019-02-07 14:19:09 -0300

    Fix code alignment issue

--- web/wp-content/themes/bumblebee/header.php
+++ web/wp-content/themes/bumblebee/header.php
@@ -17,7 +17,6 @@ wp_enqueue_style( 'bumblebee-style-header', get_stylesheet_directory_uri() . '/h
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-	
 	<?php wp_head(); ?>
 </head>
 
