723b8f006410bc74b569effb079369482720c354
subinviswanathan <subin.viswanathan@ness.com>
2019-02-08 15:46:43 +0530

    updating code and fixing validation

--- web/wp-content/plugins/ad-stack/plugin.php
+++ web/wp-content/plugins/ad-stack/plugin.php
@@ -1,18 +1,33 @@
 <?php
-/*
+/**
+Ad Stack
+
+@package     Ad Stack
 Plugin Name: TMBI Ad Stack
 Version 1.0.0
 Description: Show ads on TMBI sites
 Author: PLT Team
 Text Domain: tmbi-ad-stack
-*/
+ */
 
-include( 'inc/ads-global-targeting.php' );
+/**
+ * Including file for global targeting parameters.
+ *
+ * @file
+ */
+require 'inc/ads-global-targeting.php';
 
+
+/**
+ *  Class Ad Stack.
+ */
 class AdStack {
 
 	const VERSION = '1.0.0';
 
+	/**
+	 *  Constructor.
+	 */
 	public function __construct() {
 		add_action( 'wp_enqueue_scripts', array( $this, 'register_scripts' ) );
 		add_filter( 'ad_options', array( $this, 'bumblebee_add_slot_name_prefix' ), 10, 1 );
@@ -21,6 +36,9 @@ class AdStack {
 	}
 
 
+	/**
+	 *  Script register.
+	 */
 	public static function register_scripts() {
 		wp_register_script( 'ad-stack', plugins_url() . '/ad-stack/js/ad-stack.js', [], self::VERSION, true );
 	}
@@ -58,6 +76,72 @@ class AdStack {
 		$vars[] .= 'variant';
 		return $vars;
 	}
-}
 
+	/**
+	 * Transforms ad options into data attributes.
+	 *
+	 * @param mixed[] $ad_options   Ad rendering options.
+	 */
+	public function bumblebee_ad_options_to_attributes( $ad_options ) {
+		return array_map(
+			function( $key ) use ( $ad_options ) {
+				$att_name = 'data-ad-' . str_replace( [ '_', ' ' ], '-', $key );
+				if ( is_bool( $ad_options[ $key ] ) ) {
+					return $ad_options[ $key ] ? $att_name : '';
+				} elseif ( is_array( $ad_options[ $key ] ) ) {
+					return $att_name . '=\'' . wp_json_encode( $ad_options[ $key ] ) . '\'';
+				}
+				return $att_name . '="' . $ad_options[ $key ] . '"';
+			},
+			array_keys( $ad_options )
+		);
+	}
+
+	/**
+	 * Renders an ad placeholder
+	 *
+	 * @param int     $id        The ad ID.
+	 * @param mixed[] $options   Ad rendering options.
+	 *      string ['sizes']     Comma separated ad sizes, in the form of WxH. Example: 320x250,300x600.
+	 *      string ['slot-name'] Ad slot name.
+	 *      string ['targeting'] Custom key-value pairs for targeting.
+	 */
+	public static function bumblebee_render_ad( $id, $options ) {
+		$ad_class = $options['class'] ?? '';
+		unset( $options['class'] );
+		$classes = [ 'ad', $ad_class ];
+		$class   = implode( ' ', $classes );
+
+		/**
+		 * Filters ad options.
+		 *
+		 * @param array $options Ad rendering options
+		 *    $options = [
+		 *      'sizes'        => (string) Comma separated ad sizes, in the form of WxH. Example: 320x250,300x600.
+		 *      'slot-name'    => (string) Ad slot name.
+		 *      'targeting'    => (string) Custom key-value pairs for targeting.
+		 *    ]
+		 */
+		$ad_options = apply_filters( 'ad_options', $options );
+
+		$markup = '<div id="' . $id . '" class="' . $class . '" ' . join( ' ', self::bumblebee_ad_options_to_attributes( $ad_options ) ) . '></div>';
+
+		// Make sure we load the Ad Stack if we're rendering ads.
+		wp_enqueue_script( 'ad-stack' );
+		echo wp_kses(
+			$markup,
+			[
+				'div' => [
+					'id'                       => [],
+					'class'                    => [],
+					'data-ad-slot-name'        => [],
+					'data-ad-sizes'            => [],
+					'data-ad-targeting'        => [],
+					'data-ad-responsive-sizes' => [],
+				],
+			]
+		);
+	}
+
+}
 $adstack = new AdStack();
--- web/wp-content/themes/bumblebee/inc/ads.php
+++ web/wp-content/themes/bumblebee/inc/ads.php
@@ -15,59 +15,10 @@
  *      string ['targeting'] Custom key-value pairs for targeting.
  */
 function bumblebee_render_ad( $id, $options ) {
-	$ad_class = $options['class'] ?? '';
-	unset( $options['class'] );
-	$classes = [ 'ad', $ad_class ];
-	$class   = implode( ' ', $classes );
-
-	/**
-	 * Filters ad options.
-	 *
-	 * @param array $options Ad rendering options
-	 *    $options = [
-	 *      'sizes'        => (string) Comma separated ad sizes, in the form of WxH. Example: 320x250,300x600.
-	 *      'slot-name'    => (string) Ad slot name.
-	 *      'targeting'    => (string) Custom key-value pairs for targeting.
-	 *    ]
-	 */
-	$ad_options = apply_filters( 'ad_options', $options );
-
-	$markup = '<div id="' . $id . '" class="' . $class . '" ' . join( ' ', bumblebee_ad_options_to_attributes( $ad_options ) ) . '></div>';
-
-	// Make sure we load the Ad Stack if we're rendering ads.
-	wp_enqueue_script( 'ad-stack' );
-	echo wp_kses(
-		$markup,
-		[
-			'div' => [
-				'id'                       => [],
-				'class'                    => [],
-				'data-ad-slot-name'        => [],
-				'data-ad-sizes'            => [],
-				'data-ad-targeting'        => [],
-				'data-ad-responsive-sizes' => [],
-			],
-		]
-	);
+	if ( class_exists( 'AdStack' ) ) {
+		AdStack::bumblebee_render_ad( $id, $options );
+	}
 }
-/**
- * Transforms ad options into data attributes.
- *
- * @param mixed[] $ad_options   Ad rendering options.
- */
-function bumblebee_ad_options_to_attributes( $ad_options ) {
-	return array_map(
-		function( $key ) use ( $ad_options ) {
-			$att_name = 'data-ad-' . str_replace( [ '_', ' ' ], '-', $key );
-			if ( is_bool( $ad_options[ $key ] ) ) {
-				return $ad_options[ $key ] ? $att_name : '';
-			} elseif ( is_array( $ad_options[ $key ] ) ) {
-				return $att_name . '=\'' . wp_json_encode( $ad_options[ $key ] ) . '\'';
-			}
-			return $att_name . '="' . $ad_options[ $key ] . '"';
-		},
-		array_keys( $ad_options )
-	);
-}
+
 
 
