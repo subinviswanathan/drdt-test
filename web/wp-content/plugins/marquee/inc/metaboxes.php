57b31d30d9451d603b56fd3a7f1cb9c37e3e6742
Facundo Farias <contacto@facundofarias.com.ar>
2019-02-12 12:58:11 -0300

    DRDT-55: Fix code formatting

--- web/wp-content/plugins/marquee/inc/metaboxes.php
+++ web/wp-content/plugins/marquee/inc/metaboxes.php
@@ -1,70 +1,65 @@
 <?php
 
 class Marquee_Metaboxes extends WP_CPT_Base {
-	const post_type             = 'marquee';
-	const VERSION               = '0.2.3';
-	const LINK_META             = '_marquee_link';
-	const FEATURED_META         = '_marquee_featured';
-	const FEATURED_META_SLOT_2  = '_marquee_featured_2';
-	const FEATURED_META_SLOT_3  = '_marquee_featured_3';
-	const CONTENT_ID_META       = '_marquee_content_id';
-	const HOMEPAGE_SLOT_SLUG    = 'homepage-marquee-slot-js';
-	const HOMEPAGE_SLOT_FILE    = '../js/homepage_slot_selector.js';
+	const post_type            = 'marquee';
+	const VERSION              = '0.2.3';
+	const LINK_META            = '_marquee_link';
+	const FEATURED_META        = '_marquee_featured';
+	const FEATURED_META_SLOT_2 = '_marquee_featured_2';
+	const FEATURED_META_SLOT_3 = '_marquee_featured_3';
+	const CONTENT_ID_META      = '_marquee_content_id';
+	const HOMEPAGE_SLOT_SLUG   = 'homepage-marquee-slot-js';
+	const HOMEPAGE_SLOT_FILE   = '../js/homepage_slot_selector.js';
 
 	// Form name for Nonce and fields
 	const form_name = 'marquee-fields';
 
-	var $meta_forms = array(
+	public $meta_forms = array(
 		'marquee_options' => array(
-			'#label' => 'Marquee Options',
-
-			self::LINK_META => array(
-				'#type' => 'text',
+			'#label'                   => 'Marquee Options',
+			self::LINK_META            => array(
+				'#type'  => 'text',
 				'#class' => 'widefat',
 				'#label' => 'Marquee Link',
 			),
-
-			self::FEATURED_META => array(
-				'#type' => 'checkbox',
-				'#checked' => 'Yes',
-				'#label' => 'Featured on Homepage?',
-				'#class' => 'homepage_slots',
+			self::FEATURED_META        => array(
+				'#type'        => 'checkbox',
+				'#checked'     => 'Yes',
+				'#label'       => 'Featured on Homepage?',
+				'#class'       => 'homepage_slots',
 				'#description' => 'Check here to set the first slot content of home page',
 			),
-
 			self::FEATURED_META_SLOT_2 => array(
-				'#type' => 'checkbox',
-				'#checked' => 'Yes',
-				'#label' => 'HOME PAGE SLOT 2',
-				'#class' => 'homepage_slots',
+				'#type'        => 'checkbox',
+				'#checked'     => 'Yes',
+				'#label'       => 'HOME PAGE SLOT 2',
+				'#class'       => 'homepage_slots',
 				'#description' => 'Check here to set the second slot content of home page',
 			),
-
 			self::FEATURED_META_SLOT_3 => array(
-				'#type' => 'checkbox',
-				'#checked' => 'Yes',
-				'#label' => 'HOME PAGE SLOT 3',
-				'#class' => 'homepage_slots',
+				'#type'        => 'checkbox',
+				'#checked'     => 'Yes',
+				'#label'       => 'HOME PAGE SLOT 3',
+				'#class'       => 'homepage_slots',
 				'#description' => 'Check here to set the third slot content of home page',
 			),
-
-			self::CONTENT_ID_META => array(
-				'#type' => 'text',
-				'#class' => 'widefat',
-				'#label' => 'Content ID',
+			self::CONTENT_ID_META      => array(
+				'#type'        => 'text',
+				'#class'       => 'widefat',
+				'#label'       => 'Content ID',
 				'#description' => 'Content ID of the post, collection or recipe is required in order for this same content to not appear in another slot on the archive page.',
 			),
 
 		),
 	);
 
-	function init() {
-		add_action( 'admin_enqueue_scripts' , array( $this, 'enqueue_scripts' ) );
+	public function init() {
+		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
 	}
 
 	public function enqueue_scripts( $hook ) {
-		if ( 'post-new.php' === $hook || 'post.php' === $hook  ) {
-			wp_register_script( self::HOMEPAGE_SLOT_SLUG, plugin_dir_url( __FILE__ ) . self::HOMEPAGE_SLOT_FILE, array( 'jquery' ), self::VERSION );
+		if ( 'post-new.php' === $hook || 'post.php' === $hook ) {
+			wp_register_script( self::HOMEPAGE_SLOT_SLUG, plugin_dir_url( __FILE__ ) . self::HOMEPAGE_SLOT_FILE, array( 'jquery' ), self::VERSION, true );
 			wp_enqueue_script( self::HOMEPAGE_SLOT_SLUG );
 		}
 	}
--- web/wp-content/plugins/marquee/inc/post-type.php
+++ web/wp-content/plugins/marquee/inc/post-type.php
@@ -7,7 +7,7 @@
  * @package marquee
  */
 
-if ( ! function_exists('register_marquee_post_type') ) {
+if ( ! function_exists( 'register_marquee_post_type' ) ) {
 	function register_marquee_post_type() {
 		$labels = array(
 			'name'                  => _x( 'Marquees', 'Post Type General Name', 'marquee' ),
@@ -38,27 +38,27 @@ if ( ! function_exists('register_marquee_post_type') ) {
 			'items_list_navigation' => __( 'Marquees list navigation', 'marquee' ),
 			'filter_items_list'     => __( 'Filter items list', 'marquee' ),
 		);
-		$args = array(
-			'label'                 => __( 'Marquee', 'marquee' ),
-			'description'           => __( 'A custom post type for Marquees.', 'marquee' ),
-			'labels'                => $labels,
-			'supports'              => array( 'title', 'editor', 'revisions' ),
-			'taxonomies'            => array( 'category', 'post_tag' ),
-			'hierarchical'          => false,
-			'public'                => true,
-			'show_ui'               => true,
-			'show_in_menu'          => true,
-			'menu_position'         => 20,
-			'menu_icon'             => 'dashicons-megaphone',
-			'show_in_admin_bar'     => true,
-			'show_in_nav_menus'     => true,
-			'can_export'            => true,
-			'has_archive'           => false,
-			'exclude_from_search'   => true,
-			'publicly_queryable'    => false,
-			'rewrite'               => false,
-			'capability_type'       => 'post',
-			'show_in_rest'          => true,
+		$args   = array(
+			'label'               => __( 'Marquee', 'marquee' ),
+			'description'         => __( 'A custom post type for Marquees.', 'marquee' ),
+			'labels'              => $labels,
+			'supports'            => array( 'title', 'editor', 'revisions' ),
+			'taxonomies'          => array( 'category', 'post_tag' ),
+			'hierarchical'        => false,
+			'public'              => true,
+			'show_ui'             => true,
+			'show_in_menu'        => true,
+			'menu_position'       => 20,
+			'menu_icon'           => 'dashicons-megaphone',
+			'show_in_admin_bar'   => true,
+			'show_in_nav_menus'   => true,
+			'can_export'          => true,
+			'has_archive'         => false,
+			'exclude_from_search' => true,
+			'publicly_queryable'  => false,
+			'rewrite'             => false,
+			'capability_type'     => 'post',
+			'show_in_rest'        => true,
 		);
 		register_post_type( 'marquee', $args );
 	}
