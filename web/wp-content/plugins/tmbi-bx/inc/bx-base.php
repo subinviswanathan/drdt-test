6c7ac5277280344fee7f9eec8471a2d6ed03c37f
subinviswanathan <subin.viswanathan@ness.com>
2019-02-15 15:49:30 +0530

    Port bounce xchange from rdnap

---
+++ web/wp-content/plugins/tmbi-bx/inc/bx-base.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: MJ
+ * Date: 3/28/2017
+ * Time: 1:10 PM
+ */
+class BX_Base {
+	const FILE_SPEC         = __DIR__;
+	const LOCALIZED_LABEL   = 'rd_bx';
+	const LOADER_SCRIPT     = 'js/smart-tag.js';
+	const LOADER_LABEL      = 'fhm-bx-loader';
+	const SCRIPT_ID         = 'default';
+	const DEPENDS           = '';
+	static public $version;
+
+	public function set_version( $version ) {
+		static::$version = $version;
+	}
+
+	public function render_bx_load_failure() {
+		print( '<!-- bounce exchange script has failed to load -->' . PHP_EOL );
+	}
+
+	public function render_bx_is_blocked() {
+		print( '<!-- Bounce Exchange has been blocked -->' . PHP_EOL );
+	}
+
+	public function init() {
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+	}
+
+	public function enqueue_scripts() {
+		wp_register_script(
+			static::LOADER_LABEL,
+			self::get_asset_url( static::LOADER_SCRIPT ),
+			static::DEPENDS,
+			static::$version,
+			static::IN_FOOTER
+		);
+		$localized_data = array(
+			'script_id' => static::SCRIPT_ID,
+		);
+		wp_localize_script( static::LOADER_LABEL, static::LOCALIZED_LABEL, $localized_data );
+		wp_enqueue_script( static::LOADER_LABEL );
+	}
+
+}
---
+++ web/wp-content/plugins/tmbi-bx/inc/cpt-bx.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: P7111439
+ * Date: 9/13/2018
+ * Time: 1:09 PM
+ */
+
+class CPT_BX extends BX_Base {
+	const SCRIPT_ID = 3125;
+}
