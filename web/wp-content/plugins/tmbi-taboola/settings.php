d2151e56cd4e8fb891ad2433d53ae0e1292d7b0a
Facundo Farias <contacto@facundofarias.com.ar>
2019-02-18 12:29:28 -0300

    DRDT-86: Comment settings page code and remove superflous white space

--- web/wp-content/plugins/tmbi-taboola/settings.php
+++ web/wp-content/plugins/tmbi-taboola/settings.php
@@ -2,22 +2,24 @@
 add_action( 'admin_menu', 'tmbi_taboola_add_admin_menu' );
 add_action( 'admin_init', 'tmbi_taboola_settings_init' );
 
-
+/**
+ * Add TMBI Taboola menu link
+ */
 function tmbi_taboola_add_admin_menu() {
 	add_options_page( 'TMBI Taboola', 'TMBI Taboola', 'manage_options', 'tmbi_taboola', 'tmbi_taboola_options_page' );
 }
 
-
+/**
+ * Register Taboola settings
+ */
 function tmbi_taboola_settings_init() {
 	register_setting( 'taboola_publisher_id', 'tmbi_taboola_settings' );
-
 	add_settings_section(
 		'tmbi_taboola_taboola_integration_section',
 		__( 'Taboola integration settings', 'taboola' ),
 		'tmbi_taboola_settings_section_callback',
 		'taboola_integration'
 	);
-
 	add_settings_field(
 		'taboola_publisher_id',
 		__( 'Publisher ID', 'taboola' ),
@@ -25,11 +27,11 @@ function tmbi_taboola_settings_init() {
 		'taboola_integration',
 		'tmbi_taboola_taboola_integration_section'
 	);
-
-
 }
 
-
+/**
+ * Render Publisher ID field
+ */
 function tmbi_taboola_publisher_id_render() {
 	$options = get_option( 'tmbi_taboola_settings' );
 	$taboola_publisher_id = ! empty( $options['taboola_publisher_id'] ) ? $options['taboola_publisher_id'] : '';
@@ -39,26 +41,25 @@ function tmbi_taboola_publisher_id_render() {
 
 }
 
-
+/**
+ * Add help text to Taboola integration settings
+ */
 function tmbi_taboola_settings_section_callback() {
 	echo __( 'Configure Taboola integration', 'taboola' );
 }
 
-
+/**
+ * Render options page
+ */
 function tmbi_taboola_options_page() {
-
 	?>
 	<form action='options.php' method='post'>
-
 		<h2>TMBI Taboola</h2>
-
 		<?php
 		settings_fields( 'taboola_publisher_id' );
 		do_settings_sections( 'taboola_integration' );
 		submit_button();
 		?>
-
 	</form>
 	<?php
-
 }
