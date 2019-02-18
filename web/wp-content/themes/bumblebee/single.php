fea810bd0334d4d5d6b6ef15cf5c6978b5a8ae36
Facundo Farias <contacto@facundofarias.com.ar>
2019-02-18 11:09:12 -0300

    DRDT-86: Fix theme markup

--- web/wp-content/themes/bumblebee/single.php
+++ web/wp-content/themes/bumblebee/single.php
@@ -51,99 +51,101 @@ add_filter(
 		?>
 		</div>
 		<div class="pure-g opening-content">
-		<?php
-		if ( have_posts() ) :
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-				get_template_part( 'template-parts/content', 'post' );
-			endwhile;
-		endif;
-		?>
-		<div class="pure-u-md-7-24 pure-u-lg-7-24 pure-u-xl-7-24 hide-on-mobile article-sidebar">
-			<div class="sidebar-ad-wrapper text-center">
-				<aside class="sidebar">
-					<div class="article-sidebar-top-ad">
-						<?php
-						bumblebee_render_ad(
-							uniqid( 'ad' ),
-							[
-								'slot-name'        => 'railtop',
-								'targeting'        => [
-									'pos'      => 'railtop',
-									'location' => 'rail',
-									'tf'       => 'atf',
-								],
-								'responsive-sizes' => [
-									'mobile'       => [],
-									'tablet'       => [],
-									'desktop'      => [],
-									'large_screen' => [ [ 300, 250 ] ],
-								],
-							]
-						);
-						?>
-					</div>
-					<div class="article-sidebar-middle-ad">
-						<?php
-						bumblebee_render_ad(
-							uniqid( 'ad' ),
-							[
-								'slot-name'        => 'railmiddle',
-								'targeting'        => [
-									'pos'      => 'railmiddle',
-									'location' => 'rail',
-									'tf'       => 'atf',
-								],
-								'responsive-sizes' => [
-									'mobile'       => [],
-									'tablet'       => [],
-									'desktop'      => [],
-									'large_screen' => [ [ 300, 250 ], [ 300, 600 ] ],
-								],
-							]
-						);
-						?>
-					</div>
-					<div class="article-sidebar-scroll-ad">
-						<?php
-						bumblebee_render_ad(
-							uniqid( 'ad' ),
-							[
-								'slot-name'        => 'railscroll',
-								'targeting'        => [
-									'pos'      => 'railscroll',
-									'location' => 'rail',
-									'tf'       => 'btf',
-								],
-								'responsive-sizes' => [
-									'mobile'       => [],
-									'tablet'       => [],
-									'desktop'      => [],
-									'large_screen' => [ [ 160, 600 ], [ 300, 250 ], [ 300, 600 ], [ 300, 1050 ] ],
-								],
-							]
-						);
-						?>
-					</div>
-				</aside>
-			</div>
-		</div>
-		<div class="postarticle_ad">
+		<div class="pure-g opening-content">
 			<?php
-			bumblebee_render_ad(
-				uniqid( 'ad' ),
-				[
-					'slot-name'        => 'postarticle',
-					'responsive-sizes' => [
-						'mobile'       => [ [ 320, 50 ], [ 300, 250 ], [ 3, 3 ] ],
-						'tablet'       => [ [ 320, 50 ], [ 300, 250 ], [ 3, 3 ] ],
-						'desktop'      => [ [ 728, 90 ], [ 640, 360 ], [ 3, 3 ], [ 300, 250 ] ],
-						'large_screen' => [ [ 970, 550 ], [ 970, 250 ], [ 970, 90 ], [ 728, 90 ], [ 3, 3 ], [ 300, 250 ] ],
-					],
-				]
-			);
+			if ( have_posts() ) :
+				/* Start the Loop */
+				while ( have_posts() ) :
+					the_post();
+					get_template_part( 'template-parts/content', 'post' );
+				endwhile;
+			endif;
 			?>
+			<div class="pure-u-md-7-24 pure-u-lg-7-24 pure-u-xl-7-24 hide-on-mobile article-sidebar">
+				<div class="sidebar-ad-wrapper text-center">
+					<aside class="sidebar">
+						<div class="article-sidebar-top-ad">
+							<?php
+							bumblebee_render_ad(
+								uniqid( 'ad' ),
+								[
+									'slot-name'        => 'railtop',
+									'targeting'        => [
+										'pos'      => 'railtop',
+										'location' => 'rail',
+										'tf'       => 'atf',
+									],
+									'responsive-sizes' => [
+										'mobile'       => [],
+										'tablet'       => [],
+										'desktop'      => [],
+										'large_screen' => [ [ 300, 250 ] ],
+									],
+								]
+							);
+							?>
+						</div>
+						<div class="article-sidebar-middle-ad">
+							<?php
+							bumblebee_render_ad(
+								uniqid( 'ad' ),
+								[
+									'slot-name'        => 'railmiddle',
+									'targeting'        => [
+										'pos'      => 'railmiddle',
+										'location' => 'rail',
+										'tf'       => 'atf',
+									],
+									'responsive-sizes' => [
+										'mobile'       => [],
+										'tablet'       => [],
+										'desktop'      => [],
+										'large_screen' => [ [ 300, 250 ], [ 300, 600 ] ],
+									],
+								]
+							);
+							?>
+						</div>
+						<div class="article-sidebar-scroll-ad">
+							<?php
+							bumblebee_render_ad(
+								uniqid( 'ad' ),
+								[
+									'slot-name'        => 'railscroll',
+									'targeting'        => [
+										'pos'      => 'railscroll',
+										'location' => 'rail',
+										'tf'       => 'btf',
+									],
+									'responsive-sizes' => [
+										'mobile'       => [],
+										'tablet'       => [],
+										'desktop'      => [],
+										'large_screen' => [ [ 160, 600 ], [ 300, 250 ], [ 300, 600 ], [ 300, 1050 ] ],
+									],
+								]
+							);
+							?>
+						</div>
+					</aside>
+				</div>
+			</div>
+			<div class="postarticle_ad">
+				<?php
+				bumblebee_render_ad(
+					uniqid( 'ad' ),
+					[
+						'slot-name'        => 'postarticle',
+						'responsive-sizes' => [
+							'mobile'       => [ [ 320, 50 ], [ 300, 250 ], [ 3, 3 ] ],
+							'tablet'       => [ [ 320, 50 ], [ 300, 250 ], [ 3, 3 ] ],
+							'desktop'      => [ [ 728, 90 ], [ 640, 360 ], [ 3, 3 ], [ 300, 250 ] ],
+							'large_screen' => [ [ 970, 550 ], [ 970, 250 ], [ 970, 90 ], [ 728, 90 ], [ 3, 3 ], [ 300, 250 ] ],
+						],
+					]
+				);
+				?>
+			</div>
 		</div>
 	</main>
 </div>
