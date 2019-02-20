b19eeda2e0f8913e829362576d6d55e4bf7b432b
subinviswanathan <subin.viswanathan@ness.com>
2019-02-20 18:27:42 +0530

    remove version from hooks and prebid

--- web/wp-content/plugins/tmbi-prebid/class-prebid-js.php
+++ web/wp-content/plugins/tmbi-prebid/class-prebid-js.php
@@ -2,7 +2,7 @@
 /**
  *  * Prebid
  *
- *  @package     Prebid
+ * @package     Prebid
  * Plugin Name: TMBI Prebid
  * Description: Integrates Prebid.js with TMBI Ad Stack
  * Plugin URI: https://rd.com
@@ -35,8 +35,8 @@ class Prebid_JS {
 	 *  Init.
 	 */
 	public static function init() {
-		add_action( 'wp_enqueue_scripts', array( __CLASS__, 'enqueue_scripts' ), 10, 1 );
-		add_action( 'wp_footer', array( __CLASS__, 'remove_prebid_js' ), 1 );
+		add_action( 'wp_enqueue_scripts', array(__CLASS__, 'enqueue_scripts'), 10, 1 );
+		add_action( 'wp_footer', array(__CLASS__, 'remove_prebid_js'), 1 );
 	}
 
 	/**
@@ -44,10 +44,10 @@ class Prebid_JS {
 	 */
 	public static function enqueue_scripts() {
 		// @todo: Prebid.js needs to be built with the adapters we want (to avoid loading 200+ bidders that won't be used)
-		wp_enqueue_script( 'wp-hooks-js', plugins_url( 'js/wp-js-hooks.js', __FILE__ ), array(), self::VERSION, true );
-		wp_enqueue_script( 'prebidjs', plugins_url( 'js/prebid.js', __FILE__ ), array(), self::VERSION, true );
+		wp_enqueue_script( 'wp-hooks-js', plugins_url( 'js/wp-js-hooks.js', __FILE__ ), array(), '1.0.0', true );
+		wp_enqueue_script( 'prebidjs', plugins_url( 'js/prebid.js', __FILE__ ), array(), '1.0.0', true );
 		$prebid_js_options = get_option( 'prebid_js', false );
-		wp_register_script( 'tmbi-prebidjs', plugins_url( 'js/tmbi-prebid.js', __FILE__ ), array( 'prebidjs', 'wp-hooks-js' ), self::VERSION, true );
+		wp_register_script( 'tmbi-prebidjs', plugins_url( 'js/tmbi-prebid.js', __FILE__ ), array('prebidjs', 'wp-hooks-js'), self::VERSION, true );
 		wp_localize_script( 'tmbi-prebidjs', 'prebid_conf', $prebid_js_options );
 		wp_enqueue_script( 'tmbi-prebidjs' );
 	}
@@ -64,4 +64,5 @@ class Prebid_JS {
 		}
 	}
 }
-add_action( 'init', array( 'Prebid_JS', 'init' ) );
+
+add_action( 'init', array('Prebid_JS', 'init') );
